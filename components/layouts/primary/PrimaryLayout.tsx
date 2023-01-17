7a30ae97f0c44f3d8045a5550c4fd9850c5b36d0
Adarsh <adarsh@techwarelab.com>
2023-01-17 09:16:16 +0530

    build: layout structure

---
+++ components/layouts/primary/PrimaryLayout.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+import styles from './PrimaryLayout.module.css';
+import { PropsWithChildren } from 'react';
+
+export interface IPrimaryLayout {}
+
+const PrimaryLayout: React.FC<PropsWithChildren<IPrimaryLayout>> = ({ children }) => {
+  return (
+    <>
+      <Head>
+        <title>Primary Layout Example</title>
+      </Head>
+      <main className={styles.main}>{children}</main>
+    </>
+  );
+};
+
+export default PrimaryLayout;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,16 @@
-import CssBaseline from '@material-ui/core/CssBaseline';
 import type { AppProps } from 'next/app';
-import Head from 'next/head';
-import * as React from 'react';
-import '../styles/globals.css';
+import './globals.css';
+import { NextPageWithLayout } from './page';
 
-export default function App({ Component, pageProps }: AppProps) {
-  React.useEffect(() => {
-    const jssStyles = document.querySelector('#jss-server-side');
-    if (jssStyles) {
-      jssStyles.parentElement?.removeChild(jssStyles);
-    }
-  }, []);
-
-  return (
-    <React.Fragment>
-      <Head>
-        <title>Saas App</title>
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width"
-        />
-      </Head>
-
-      <CssBaseline />
-      <Component {...pageProps} />
-    </React.Fragment>
-  );
+interface AppPropsWithLayout extends AppProps {
+  Component: NextPageWithLayout;
 }
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  // Use the layout defined at the page level, if available
+  const getLayout = Component.getLayout || ((page) => page);
+
+  return getLayout(<Component {...pageProps} />);
+}
+
+export default MyApp;
